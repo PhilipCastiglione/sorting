9965ed29ab0adaf0ed8bdf3a75433886f8e07e4c
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2015-06-29 17:31:23 +1000

    merge update

--- merge.rb
+++ merge.rb
@@ -9,23 +9,37 @@ def merge(ary)
     split_ary.flatten!(1)
   end
   # return split_ary
-  merge_ary = order(split_ary, 1)
-
+  merge_ary = split_ary
+  for i in 1..1 #change this to the correct #
+    merge_ary = order(merge_ary, i)
+  end
 end
 
 def order(ary, iteration)
   new_ary = []
   for i in 0..(ary.size / 2**iteration - 1)
+    sub_ary = []
     list1 = ary[2 * i]
     idx1 = 0
     list2 = ary[2* i + 1]
     idx2 = 0
-    if list1[idx1] > list2[idx2]
-      #push list2idx2 into sub list
-
-    else
-
+    
+    def compare
+      # if list 1 is empty go within list 2
+      # if list 2 is empty go within list 1
+      # this for both lists are not empty
+      if list1[idx1] > list2[idx2]
+        #push list2idx2 into sub list
+        sub_ary << list2[idx2]
+        idx2 += 1
+      else
+        sub_ary << list1[idx1]
+        idx1 += 1
+      end
+      # if there's only one left, use that
+      # otherwise, recurse
     end
+
   end
   
 end
@@ -35,3 +49,5 @@ def split(ary)
 end
 
 p merge([5,4,6,2,3,1,7,8])
+
+# goddamn refactor
